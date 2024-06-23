de38a8d94c5ded6b6ce791de21f55f5a39c77b3d
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-06-24 10:32:36 +1200

    Saves tasks to local storage.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,11 +9,14 @@
   function removeTask(index) {
     tasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)]
   }
+  function saveTasks() {
+    localStorage.todos = JSON.stringify(tasks)
+  }
 </script>
 
 <Header />
 <button on:click={addTask}>📝 Add</button>
-
+<button on:click={saveTasks}>💾 Save</button>
 {#each tasks as task, index}
   <div class="task">
     <input bind:value={tasks[index]} />
