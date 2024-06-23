a054f233dd49583fb8f90154c44eb3277023eb50
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-06-24 10:25:54 +1200

    Removes tasks from the array.
    tested on chrome

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,23 @@
   function addTask() {
     tasks = [...tasks, '']
   }
+  function removeTask(index) {
+    tasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)]
+  }
 </script>
 
 <Header />
-
 <button on:click={addTask}>📝 Add</button>
+
 {#each tasks as task, index}
   <div class="task">
     <input bind:value={tasks[index]} />
+
+    <button
+      on:click={() => {
+        removeTask(index)
+      }}>🗑</button
+    >
   </div>
 {/each}
 
