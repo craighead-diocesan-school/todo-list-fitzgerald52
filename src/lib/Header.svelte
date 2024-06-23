ee208f3ba2a3c96319abc4f1cc925104589da42e
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-06-24 10:13:44 +1200

    Adds tasks to array
    tested on chrome

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,1 @@
-<header class="hero">
-  <div class="hero-body">
-    <h1 class="title">Welcome</h1>
-    <h2 class="subtitle">Welcome to my site</h2>
-  </div>
-</header>
+<header class="hero"></header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,24 @@
 <script>
   import Header from '$lib/Header.svelte'
+
+  let tasks = []
+
+  function addTask() {
+    tasks = [...tasks, '']
+  }
 </script>
 
 <Header />
 
-<main>
-  <h2>SvelteKit</h2>
+<button on:click={addTask}>📝 Add</button>
+{#each tasks as task, index}
+  <div class="task">
+    <input bind:value={tasks[index]} />
+  </div>
+{/each}
 
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
-</main>
-
-<footer>
-  <p>&copy; Craighead Diocesan School 2024</p>
-</footer>
+<style>
+  .task {
+    display: block;
+  }
+</style>
